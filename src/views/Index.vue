c25a9c87b55cde385b75827ba503339d80146a2e
soakit <liuchang2024@gmail.com>
2021-03-23 16:42:58 +0800

    feat: support vuex and ssr

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,3 +1,13 @@
 <template>
-    <div>首页</div>
+  <div>
+    <h2>Index Page</h2>
+    <p>num: {{ $store.state.count }}</p>
+    <button @click="$store.commit('add')">add</button>
+  </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
